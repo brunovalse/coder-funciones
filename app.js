f526e6bd043c5a6dc0cf2027785df048b022486b
Bruno Valsecchi <mbrunovalsecchi@gmail.com>
2021-10-06 22:52:32 -0300

    arreglando funciones

--- app.js
+++ app.js
@@ -1,18 +1,42 @@
-function carrito() {
+function carritoRevistas() {
     let cantidadRevistas = prompt("¿Cuantas revistas desea?");
+    
+    alert("Usted a pedido la cantidad de " + cantidadRevistas + " revistas");
+
+
+}
+carritoRevistas();
+
+function carritoLibros(){
     let cantidadLibros = prompt("¿Cuantos libros desea?");
-    alert("Usted a pedido la cantidad de " + cantidadRevistas + " revistas y de libros " + cantidadLibros + " a su pedido");
-}
-carrito();
 
-function sumaTotal() {
-    let valorTotal = revistas + libros ;
-    return valorTotal;
+    alert("Usted a pedido la cantidad de " + cantidadLibros + " libros");
+
+}
+carritoLibros   ();
+
+function sumaRevistas() {
+    let precioRevistas = 50 ;
+    let valorRevistas = precioRevistas * carritoRevistas ;
+    return valorRevistas;
+}
+sumaRevistas()
+function sumaLibros() {    
+    let precioLibros = 200 ;
+
+    let valorLibros = precioLibros * carritoLibros ;
+    return valorLibros;
+}
+sumaLibros()
+
+function sumaTotal(){
+    let valorTotal = sumaLibros +sumaRevistas ;
+    return valorTotal
 }
 
-let revistas = 50 ;
-let libros = 200 ;
-let compra = sumaTotal (revistas + libros);
+
+
+let compra = sumaTotal (sumaLibros + sumaRevistas);
 
 console.log("el valor total de la compra realizada es de $" + compra)
 console.log ("Muchas Gracias, por realizar la compra, espero que le guste.");
